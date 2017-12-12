86d8daaf6ea99ddfd886ef4f3b09019b2bf38706
Shashvat Aditya <shashvataditya123@gmail.com>
2017-12-13 03:01:14 +0530

    Added username to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-	attr_accessor :name, :email, :password
+	attr_accessor :name, :username, :email, :password
 	has_many :followers, :class_name => 'Follower', :foreign_key => 'followee_id'
   	has_many :followees, :class_name => 'Follower', :foreign_key => 'follower_id'
 	has_one :profile
--- db/migrate/20171212161608_create_users.rb
+++ db/migrate/20171212161608_create_users.rb
@@ -4,6 +4,7 @@ class CreateUsers < ActiveRecord::Migration
 	  t.string "email", :limit => 50, :null => false
       t.column "password", :string, :limit => 50, :null => false
       t.string "name", :limit => 50, :null => false
+      t.string "username", :limit => 50, :null => false
 
       t.timestamps
     end
