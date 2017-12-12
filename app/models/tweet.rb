ce56ff6054cdf403b4070cdb78cbd0013ee98daa
Shashvat Aditya <shashvataditya123@gmail.com>
2017-12-13 03:44:46 +0530

    Added Validations

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,4 +1,6 @@
 class Tweet < ActiveRecord::Base
 	attr_accessor :content
 	belongs_to :user
+
+	validates :content, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,11 @@ class User < ActiveRecord::Base
   	has_many :followees, :class_name => 'Follower', :foreign_key => 'follower_id'
 	has_one :profile
 	has_many :tweets
+
+	validates :name, presence: true
+	validates :username, presence: true, uniqueness: true
+	validates :email, presence: true, uniqueness: true
+	validates :password, presence: true, 
+				format: { with: /^(?=.*[A-Za-z]).{8,}$/,
+				message: "Password doesn't satisfy the rules" }
 end
